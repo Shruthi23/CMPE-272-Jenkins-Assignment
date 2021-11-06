1f6379f9ee3ba061edcca4fb7df2edfdfd573206
shruthi2306 <shruthi.srinivasan01@sjsu.edu>
2021-11-06 12:39:45 -0700

    Hello api added

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ app.get('/',function(req,res)
 {
 res.send('Hello World!');
 });
+app.get('/hello',function(req,res)
+{
+res.send('Good Morning!!');
+});
 var server=app.listen(3000,function() {
-console.log("Start server")
+console.log("Start server ")
 
 });
